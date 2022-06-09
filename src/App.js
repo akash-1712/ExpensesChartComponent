65eb0dc76c8ebe329d97ffa94bc4e2cf0a3a6310
akash-1712 <akashbansalab01@gmail.com>
2022-06-09 12:16:04 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./app.module.scss";
+
+function App() {
+  return (
+    <div className={style.content}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
